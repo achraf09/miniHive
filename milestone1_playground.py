2514b962ed9564d04dd39f007fe33ebdce90e074
ashraf09 <ashraf.ragui@gmail.com>
2020-12-03 19:21:38 +0100

    second commit

--- milestone1_playground.py
+++ milestone1_playground.py
@@ -40,34 +40,40 @@ def sql_tokenize(string):
 
     return newtokens
 
-
-sql = "select distinct * from Person"
+stmt_dict={}
+sql = "Select distinct name From Person, Eats Where age=16"
 stmt = sqlparse.parse(sql)[0]
 flat_tokens = sqlparse.sql.TokenList(stmt.tokens).flatten()
 for token in flat_tokens:
-    if token.ttype in tokens.Whitespace:
+    if token.ttype in tokens.Whitespace or 'DISTINCT' == token.normalized:
         continue
     else:
-        print(token.ttype)
+        type_token = token.ttype
         token = str(token).strip()
-        print(token)
+        token_string = token
+        print(f"{type_token}, {token_string}")
         print("=====")
         #print(sqlparse.keywords.is_keyword(token))
 print("#################-----")
 
 #print(sqlparse.sql.Comparison(stmt.tokens))
 for token in stmt.tokens:
-    print(token)
-    print(token.ttype)
-
-print(stmt.tokens[-1])
+    if token.ttype in tokens.Whitespace:
+        continue
+    else:
+        print(token)
+        print(token.ttype)
+test = [token.split('from')[0] for token in flat_tokens]
+print(test)
+#print(stmt.tokens[-1])
 print("--------------")
-
-cond = radb.ast.ValExprBinaryOp(radb.ast.AttrRef(None, 'age'), radb.ast.sym.EQ, radb.ast.RANumber('16'))
-input = radb.ast.RelRef('Person')
-select = radb.ast.Select(cond, input)
-expected = radb.parse.one_statement_from_string("\select_{age=16}(Person);")
-print(select) # Output
-print(expected)
+print(stmt.tokens[0])
+#
+# cond = radb.ast.ValExprBinaryOp(radb.ast.AttrRef(None, 'age'), radb.ast.sym.EQ, radb.ast.RANumber('16'))
+# input = radb.ast.RelRef('Person')
+# select = radb.ast.Select(cond, input)
+# expected = radb.parse.one_statement_from_string("Person;")
+# print(select) # Output
+# print(expected)
 #for token in flat_tokens:
 #    if token select and
--- sql2ra.py
+++ sql2ra.py
@@ -2,6 +2,39 @@ import sqlparse
 import radb
 import radb.ast
 import radb.parse
+from sqlparse import tokens
 
+stmt_dict={}
 def translate(stmt):
-    pass
+
+    index = 0
+    stmt = sqlparse.parse(stmt)[0]
+    for token in stmt.tokens:
+        if token.ttype in tokens.Whitespace or 'DISTINCT' == token.normalized:
+            index += 1
+            continue
+        else:
+            print(token.ttype)
+            if token.ttype in tokens.Keyword or 'WHERE' in token.normalized:
+
+                stmt_dict[str(token).upper()] = get_keyword_attribute(stmt, token, index)
+                token = str(token).upper()
+            index += 1
+            print(token)
+    print(stmt_dict)
+def get_keyword_attribute(stmt,key,index):
+    list_items=[]
+    for token in stmt.tokens[index+1:]:
+        if token.ttype in tokens.Whitespace or 'DISTINCT' == token.normalized:
+            continue
+        else:
+            if token.ttype not in tokens.Keyword :
+                if 'where' in str(token):
+                    continue
+                list_items.append(str(token).strip())
+            else:
+                if token.ttype in tokens.Keyword:
+                    break
+    return list_items
+
+translate("Select distinct * from Person where age = 16")
